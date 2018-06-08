a4c5b05a66aef103d5761808a78b25ae601b7a85
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-06-08 13:49:02 +0200

    LocalStorage implementation

--- src/app/core/local-storage.service.ts
+++ src/app/core/local-storage.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { CoreModule } from './core.module';
+import { Joke } from './joke';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: CoreModule
 })
 export class LocalStorageService {
+  readonly FAVORITES_KEY = 'favorites';
 
-  constructor() { }
+  constructor() {}
+
+  setFavorites(favorites: Joke[]): void {
+    localStorage.setItem(this.FAVORITES_KEY, JSON.stringify(favorites));
+  }
+
+  getFavorites(): Joke[] {
+    const favorites = localStorage.getItem(this.FAVORITES_KEY);
+
+    return JSON.parse(favorites) as Joke[];
+  }
 }
