789c8a1622b4cdda16bb810bd91ee573c85926b4
Catalin Ciubotaru <catalinciubot@gmail.com>
2018-06-10 00:32:54 +0200

    Added basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { JokeComponent } from 'src/app/joke-list/joke/joke.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { JokeListComponent } from './joke-list/joke-list.component';
 
 @NgModule({
   declarations: [AppComponent, JokeComponent, JokeListComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, CoreModule, AppRoutingModule, FormsModule, TabsModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
